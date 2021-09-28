9b16cecd7457990e1e25b2f0e13be5379759cb09
Davide Vernassa <davide.vernassa@eng.it>
2021-09-28 10:49:23 +0200

    added exercise content

---
+++ src/components/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="container">
+  test
+	</div>
+</template>
+
+<script lang="ts">
+import { createApp } from "vue";
+
+export default createApp({
+  name: "menu"
+});
+</script>
